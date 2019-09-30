a52c9443c7771476f26c1461f16dd9f49f75e6fe
Andrew Wong <awwong92@gmail.com>
2019-09-29 23:33:04 -0400

    just before adding templates

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 const Koa = require('koa');
-const app = new Koa();
+const KoaRouter = require('koa-router');
 const json = require('koa-json');
+const path = require('path');
+const render = require('koa-ejs');
+
+const app = new Koa();
+const router = new KoaRouter();
 
 // JSON prettier
 app.use(json());
 
-// Super simple koa server without routing yet
-app.use(async ctx => (ctx.body = { msg: 'Hello World' }));
+// // Super simple koa server without routing yet
+// app.use(async ctx => (ctx.body = { msg: 'Hello World' }));
+
+// Basic routes utilizing router middleware
+router.get('/', ctx => ctx.body = 'Hello, this is a basic Koa server');
+router.get('/booger', ctx => ctx.body = 'Hey, you picked this route!');
+router.get('/array', ctx => ctx.body = ['a', 'b', 'c', 'd']);
+router.get('/object', ctx => ctx.body = {can: 'easily', print: 'json', obj: 'here'});
+router.get('*', ctx => ctx.body = 'catch all route');
+
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'layout',
+  viewExt: 'html',
+  cache: false,
+  debug: true
+})
+
+// Index
+router.get('/', async ctx => {
+  await ctx.render('index');
+})
+
+// Router Middleware
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000, () => console.log('Koa server started on PORT 3000......'));
