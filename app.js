96fe8e3f522bee995da58d721926957015b53d7b
Andrew Wong <awwong92@gmail.com>
2019-09-29 23:12:05 -0400

    added middleware to make json prettier

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const Koa = require('koa');
 const app = new Koa();
+const json = require('koa-json');
+
+// JSON prettier
+app.use(json());
 
 // Super simple koa server without routing yet
-app.use(async ctx => (ctx.body = 'Hello World'));
+app.use(async ctx => (ctx.body = { msg: 'Hello World' }));
 
 app.listen(3000, () => console.log('Koa server started on PORT 3000......'));
